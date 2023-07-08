011a47d8fdeccfd2db6befd4d32a769aeba45edb
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2023-07-08 12:25:28 +0700

    use nested layout

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,25 +1,21 @@
-import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import MenuCard from "./components/MenuCard";
 
 export default function RestaurantMenu() {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[100%] rounded p-3 shadow">
-          <NavBar />
-          <main className="bg-white mt-5">
-            <div>
-              <div className="mt-4 pb-1 mb-1">
-                <h1 className="font-bold text-4xl">Menu</h1>
-              </div>
-              <div className="flex flex-wrap justify-between">
-                <MenuCard />
-              </div>
+      <div className="bg-white w-[100%] rounded p-3 shadow">
+        <NavBar />
+        <main className="bg-white mt-5">
+          <div>
+            <div className="mt-4 pb-1 mb-1">
+              <h1 className="font-bold text-4xl">Menu</h1>
             </div>
-          </main>
-        </div>
+            <div className="flex flex-wrap justify-between">
+              <MenuCard />
+            </div>
+          </div>
+        </main>
       </div>
     </>
   );
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import Description from "./components/Description";
-import Header from "./components/Header";
 import Images from "./components/Images";
 import NavBar from "./components/NavBar";
 import Rating from "./components/Rating";
@@ -10,18 +9,15 @@ import Title from "./components/Title";
 export default function RestaurantDetails() {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <NavBar />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <ReservationCard />
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <NavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
       </div>
+      <ReservationCard />
     </>
   );
 }
