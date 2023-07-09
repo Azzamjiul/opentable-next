01e7c934713ef493fc0abd9c5062570c03747f72
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2023-07-09 07:35:38 +0700

    fetching restaurant detail

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -6,13 +6,48 @@ import Rating from "./components/Rating";
 import ReservationCard from "./components/ReservationCard";
 import Reviews from "./components/Reviews";
 import Title from "./components/Title";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    }
+  });
+
+  if(!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+};
 
 export const metadata: Metadata = {
   title: 'Milesstone Grill - OpenTable',
   description: 'Some Description',
 }
 
-export default function RestaurantDetails() {
+export default async function RestaurantDetails({ params }: { params: { slug: string } }) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
+  console.log(restaurant);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
