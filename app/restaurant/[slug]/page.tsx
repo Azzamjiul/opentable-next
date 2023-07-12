7e896f6330c5c777ce236f682e8b209ae23a6580
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2023-07-12 07:23:09 +0700

    handle not found error

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -7,6 +7,7 @@ import ReservationCard from "./components/ReservationCard";
 import Reviews from "./components/Reviews";
 import Title from "./components/Title";
 import { PrismaClient, Review } from "@prisma/client";
+import { notFound } from "next/navigation";
 
 const prisma = new PrismaClient();
 
@@ -35,7 +36,7 @@ const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
   });
 
   if (!restaurant) {
-    throw new Error("Cannot find restaurant");
+    notFound();
   }
 
   return restaurant;
